4d625f7c9e9f93f46f731cd478ed5b90d479b760
Jorge <ibarrapaezj@gmail.com>
2024-12-02 15:10:23 -0500

    Despliegue sin Docker v3

--- post-build.js
+++ post-build.js
@@ -1,13 +1,17 @@
-const {
-  writeFile,
-  readFile,
-} = require("fs").promises;
+const { writeFile, readFile } = require("fs").promises;
 const { join } = require("path");
+const fs = require("fs");
 
 const main = async () => {
-  const file = await readFile(join(__dirname, "out/404/index.html"));
+  const filePath = join(__dirname, "out", "404", "index.html");
 
-  await writeFile(join(__dirname, "out/404.html"), file);
+  // Verifica si el archivo 404 existe
+  if (fs.existsSync(filePath)) {
+    const file = await readFile(filePath);
+    await writeFile(join(__dirname, "out", "404.html"), file);
+  } else {
+    console.log("El archivo 404 no se ha generado. Omitiendo...");
+  }
 };
 
 main();
